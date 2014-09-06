4b727170a0f8b4370ab53de9b2c5930fb9e906fc
Justin Gordon <justin.gordon@gmail.com>
2014-09-05 17:43:38 -1000

    Enable React devtools by exposing React (setting window.React)

--- examples/flux-chat/js/app.js
+++ examples/flux-chat/js/app.js
@@ -18,6 +18,7 @@ var ChatApp = require('./components/ChatApp.react');
 var ChatExampleData = require('./ChatExampleData');
 var ChatWebAPIUtils = require('./utils/ChatWebAPIUtils');
 var React = require('react');
+window.React = React; // export for http://fb.me/react-devtools
 
 ChatExampleData.init(); // load example data into localstorage
 
