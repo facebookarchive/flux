57b122a205b162fc117867e58d4063332fbcc562
Guy Nesher <nesher.guy@gmail.com>
2014-09-17 11:14:29 +0100

    detect missing action in dispatch

--- src/Dispatcher.js
+++ src/Dispatcher.js
@@ -186,6 +186,10 @@ class Dispatcher {
       !this._isDispatching,
       'Dispatch.dispatch(...): Cannot dispatch in the middle of a dispatch.'
     );
+    invariant(
+        typeof payload.action === 'undefined',
+        'Dispatch.dispatch(...): Cannot dispatch payload with missing action.'
+    );
     this._startDispatching(payload);
     try {
       for (var id in this._callbacks) {
