7d20be800bf4ed2549cfb5ced9a7645e3024c5cd
Riley Tomasek <riley.tomasek@gmail.com>
2014-12-22 17:54:53 -0800

    Slight refactor of message creation Move getCreatedMessageData() from the message stores getters to the ChatMessageUtils and move call to WebAPI's create method into the message store.

--- examples/flux-chat/js/actions/ChatMessageActionCreators.js
+++ examples/flux-chat/js/actions/ChatMessageActionCreators.js
@@ -24,8 +24,6 @@ module.exports = {
       type: ActionTypes.CREATE_MESSAGE,
       text: text
     });
-    var message = MessageStore.getCreatedMessageData(text);
-    ChatWebAPIUtils.createMessage(message);
   }
 
 };
