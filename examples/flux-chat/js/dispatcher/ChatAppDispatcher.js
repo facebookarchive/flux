9068c1ab4eb744c47c4b8b00116aae4fc385a845
jarsbe <jarsbe@gmail.com>
2015-03-07 10:22:51 +0700

    Reduce dispatcher to non-modified instance.

--- examples/flux-chat/js/dispatcher/ChatAppDispatcher.js
+++ examples/flux-chat/js/dispatcher/ChatAppDispatcher.js
@@ -10,38 +10,6 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-var ChatConstants = require('../constants/ChatConstants');
 var Dispatcher = require('flux').Dispatcher;
-var assign = require('object-assign');
 
-var PayloadSources = ChatConstants.PayloadSources;
-
-var ChatAppDispatcher = assign(new Dispatcher(), {
-
-  /**
-   * @param {object} action The details of the action, including the action's
-   * type and additional data coming from the server.
-   */
-  handleServerAction: function(action) {
-    var payload = {
-      source: PayloadSources.SERVER_ACTION,
-      action: action
-    };
-    this.dispatch(payload);
-  },
-
-  /**
-   * @param {object} action The details of the action, including the action's
-   * type and additional data coming from the view.
-   */
-  handleViewAction: function(action) {
-    var payload = {
-      source: PayloadSources.VIEW_ACTION,
-      action: action
-    };
-    this.dispatch(payload);
-  }
-
-});
-
-module.exports = ChatAppDispatcher;
+module.exports = new Dispatcher();
