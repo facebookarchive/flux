f10a4fe9fde144b1fdc0d3bad969cbcd71b8b455
Kyle Davis <kad@fb.com>
2016-05-14 14:28:24 -0700

    Fixes npm install on clean checkout

--- utils.js
+++ utils.js
@@ -8,7 +8,6 @@
  */
 
 module.exports.Container = require('./lib/FluxContainer');
-module.exports.MapStore = require('./lib/FluxMapStore');
 module.exports.Mixin = require('./lib/FluxMixinLegacy');
 module.exports.ReduceStore = require('./lib/FluxReduceStore');
 module.exports.Store = require('./lib/FluxStore');
