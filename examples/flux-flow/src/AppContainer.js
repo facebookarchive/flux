b70a0bfab9478ddc6f557162454df5eb998ba107
Kyle Davis <kad@fb.com>
2016-06-09 07:33:01 -0700

    Add flow annotation to flow example files

--- examples/flux-flow/src/AppContainer.js
+++ examples/flux-flow/src/AppContainer.js
@@ -5,10 +5,13 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
  */
 
 'use strict';
 
+import AppDispatcher from './AppDispatcher';
 import AppStore from './AppStore';
 import AppView from './AppView';
 import {Container} from 'flux/utils';
@@ -27,7 +30,7 @@ function getState() {
     // incorrectly formed action.
     onFooChange: () => AppDispatcher.dispatch({
       type: 'foo',
-      bar: string,
+      bar: 'Hello Bar!',
     }),
   };
 }
