fe4c63556d821c53912835569c336b939146f895
Kyle Davis <kad@fb.com>
2016-06-09 07:28:58 -0700

    Add flux-flow example

---
+++ examples/flux-flow/src/AppContainer.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+'use strict';
+
+import AppStore from './AppStore';
+import AppView from './AppView';
+import {Container} from 'flux/utils';
+
+function getStores() {
+  return [
+    AppStore,
+  ];
+}
+
+function getState() {
+  return {
+    value: AppStore.getState(),
+
+    // This is an error that should be caught by flow. Cannot dispatch an
+    // incorrectly formed action.
+    onFooChange: () => AppDispatcher.dispatch({
+      type: 'foo',
+      bar: string,
+    }),
+  };
+}
+
+export default Container.createFunctional(AppView, getStores, getState);
---
+++ examples/flux-flow/src/AppStore.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+'use strict';
+
+import type {Action} from './AppActions';
+
+import {ReduceStore} from 'flux/utils';
+import AppDispatcher from './AppDispatcher';
+
+class AppStore extends ReduceStore<string> {
+  constructor() {
+    super(AppDispatcher);
+  }
+
+  getInitialState(): string {
+    return 'Hello World!';
+  }
+
+  reduce(state: string, action: Action): string {
+    switch (action.type) {
+      case 'foo':
+        return action.foo;
+
+      // This is an error that should be caught by flow.
+      case 'bar':
+       return action.foo;
+
+      // This case is okay though, bar actions have a bar property.
+      case 'bar':
+        return action.bar;
+
+      default:
+        return state;
+    }
+  }
+}
+
+export default new AppStore();
