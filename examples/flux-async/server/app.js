4bf8ffecdaada4a2ede6bc1f723824b1252c5d32
Kyle Davis <kad@fb.com>
2016-09-07 11:11:49 -0700

    update app.js

--- examples/flux-async/server/app.js
+++ examples/flux-async/server/app.js
@@ -30,12 +30,11 @@ app.get('/', (req, res) => {
   res.send(`<pre>
 
 GET /todos
-  Returns an array of strings that are the ids of all known todos. The ids
-  are in the order they were saved. The array is empty if no todos have been
-  saved.
+  Returns an array of strings that are the ids of all known todos. The array is
+  empty if no todos have been saved.
 
-GET /todo/<id>
-  Returns the todo of the given \`id\`. The object returned has the shape:
+GET /todo/$ID
+  Returns the todo of the given $ID. The object returned has the shape:
 
     {
       id: string,
@@ -43,12 +42,16 @@ GET /todo/<id>
       complete: boolean,
     }
 
-POST /todo/<id>?text=<text>&complete=<complete>
-  Saves a todo with the given \`id\`, \`text\`, and \`complete\` status. If the
-  \`id\` corresponds to an existing todo it will be updated.
+POST /todo/create?text=$TEXT&complete=$COMPLETE
+  Creates a todo with the given $TEXT, and $COMPLETE status. The entire
+  todo, including id, will be sent back in the response.
 
-DELETE /todo/<id>
-  Removes the todo of the given \`id\`.
+POST /todo/update/$ID?text=$TEXT&complete=$COMPLETE
+  Updates a todo with the given $TET, and $COMPLETE status. The entire
+  todo, including id, will be sent back in the response.
+
+POST /todo/delete/$ID
+  Removes the todo of the given $ID.
 
 </pre>`);
 });
@@ -75,12 +78,12 @@ app.get('/todo/:id', (req, res) => {
 });
 
 /**
- * Update or create todo with given id, text, and complete.
+ * Create todo with text, and complete.
  */
-app.post('/todo/:id', (req, res) => {
+app.post('/todo/create', (req, res) => {
   const todos = getTodos();
 
-  const id = req.param.id;
+  const id = nextID();
   const text = req.query.text;
   const complete = req.query.complete;
 
@@ -101,13 +104,48 @@ app.post('/todo/:id', (req, res) => {
   };
 
   setTodos(todos);
-  res.status(200).send();
+  res.status(200).send(todos[id]);
+});
+
+/**
+ * Update todo with given id, text, and complete.
+ */
+app.post('/todo/update/:id', (req, res) => {
+  const todos = getTodos();
+
+  const id = req.params.id;
+  const text = req.query.text;
+  const complete = req.query.complete;
+
+  if (!todos[id]) {
+    res.status(404).send(`Todo "${req.params.id}" does not exist.`);
+    return;
+  }
+
+  if (text == null) {
+    res.status(400).send(`\`text\` query parameter is missing.`);
+    return;
+  }
+
+  if (complete == null) {
+    res.status(400).send(`\`complete\` query parameter is missing.`);
+    return;
+  }
+
+  todos[id] = {
+    id: id,
+    text: text,
+    complete: !!complete,
+  };
+
+  setTodos(todos);
+  res.status(200).send(todos[id]);
 });
 
 /**
  * Delete an existing todo or 404 if the id doesn't exist.
  */
-app.delete('/todo/:id', (req, res) => {
+app.post('/todo/delete/:id', (req, res) => {
   const todos = getTodos();
 
   if (!todos[req.params.id]) {
@@ -139,4 +177,22 @@ function getTodos() {
 
 function setTodos(todos) {
   fs.writeFileSync(DATA_FILE, JSON.strinify(todos, null, 2), 'utf8');
+}
+
+let max = null;
+function nextID() {
+  if (max == null) {
+    max = 0;
+    Object.keys(getTodos()).forEach(key => {
+      if (/^id_[1-9]\d*$/.test(key)) {
+        const idPart = key.split('_')[1];
+        max = Math.max(max, Number(idPart));
+      } else {
+        throw new Error(
+          `Invalid id "${key}" found, ids must look like id_<number>`
+        );
+      }
+    });
+  }
+  return 'id_' + (++max);
 }
