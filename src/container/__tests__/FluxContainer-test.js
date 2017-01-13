9223e7323c06f935b1537b7ffe412a562590c01c
Tsuyoshi Yamashita <yamashita@opcode.co.jp>
2017-01-13 10:30:39 +0900

    Add tests for getStores with context.

--- src/container/__tests__/FluxContainer-test.js
+++ src/container/__tests__/FluxContainer-test.js
@@ -34,7 +34,7 @@ function createContainer(containerClass, options, props, context) {
     // Create a new component that provides the child context.
     var ComponentWithContext = React.createClass({
       childContextTypes: {
-        value: React.PropTypes.string,
+        value: React.PropTypes.any,
       },
       getChildContext: function() {
         return context;
@@ -238,7 +238,38 @@ describe('FluxContainer', () => {
     expect(getValue()).toBe('bar');
   });
 
-  it('should get access to context', () => {
+  it('should get access to context in getStores', () => {
+    // Setup the container.
+    class SimpleContainer extends BaseContainer {
+      static contextTypes = {
+        value: React.PropTypes.array,
+      };
+
+      static getStores(props, context) {
+        return context.value;
+      }
+
+      static calculateState(prevState, props, context) {
+        return {
+          value: FooStore.getState(),
+        };
+      }
+    }
+
+    var getValue = createContainer(
+      SimpleContainer,
+      {withContext: true}, // options
+      {}, // props
+      {
+        value: [FooStore],
+      }, // context
+    );
+
+    // Test it.
+    expect(getValue()).toBe('foo');
+  });
+
+  it('should get access to context in calculateState', () => {
     // Setup the container.
     class SimpleContainer extends BaseContainer {
       static contextTypes = {
